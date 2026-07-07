6ca4d902173c940c23c34082c73dfedd3c9dc4dc
Oliver Hennhöfer <oliver.hennhoefer@mail.de>
2026-07-07 14:37:25 +0200

    Implements Astro blog

---
+++ src/site.ts
@@ -0,0 +1,15 @@
+export const site = {
+  title: "Oliver Hennhoefer",
+  description:
+    "Research notes on conformal inference, anomaly detection, uncertainty quantification, online FDR, and machine learning systems under uncertainty.",
+  url: "https://oliverhennhoefer.github.io",
+  author: "Oliver Hennhoefer",
+  email: "oliver.hennhoefer@h-ka.de",
+  affiliation: "Hochschule Karlsruhe, Intelligent Systems Research Group",
+  socials: {
+    github: "https://github.com/OliverHennhoefer",
+    linkedin: "https://linkedin.com/in/oliver-hennhoefer",
+    orcid: "https://orcid.org/0000-0001-9834-4685",
+    dblp: "https://dblp.org/pid/371/4401.html",
+  },
+};
